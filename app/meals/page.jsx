df2ad2cee295e5cefec6d6b725da4f978dc7b084
Mohamed-Gamal-22 <mogamall086@gmail.com>
2025-07-03 18:42:25 +0300

    make meals grid design all before fetching data and siplay

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import style from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function Meals() {
-  return <div>Meals</div>;
+  return (
+    <>
+      <header className={style.header}>
+        <h1>
+          Delicious Meals, Created{" "}
+          <span className={style.highlight}>By You</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun
+        </p>
+        <p className={style.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={style.main}>
+        <MealsGrid meals={[]}/> 
+      </main>
+    </>
+  );
 }
---
+++ components/meals/meals-grid.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./meals-grid.module.css";
+import MealItem from "./mealItems/meal-item";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={style.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meals} />
+        </li>
+      ))}
+    </ul>
+  );
+}
