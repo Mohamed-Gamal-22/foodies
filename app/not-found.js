cc2b852bff3445cbdaea216603452f4473250b73
Mohamed-Gamal-22 <mogamall086@gmail.com>
2025-07-05 23:07:40 +0300

    make not found page public one

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find requested page or resource.</p>
+    </div>
+  );
+}
