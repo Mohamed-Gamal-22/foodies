cdd99f9a6dc0746c0454229cfde49512645d304d
Mohamed-Gamal-22 <mogamall086@gmail.com>
2025-07-06 13:43:37 +0300

    meal details

--- app/meals/[mealDetails]/page.jsx
+++ app/meals/[mealDetails]/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import style from "./meal-details.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
-export default function MealDetails() {
+export default async function MealDetails({ params }) {
+  const meal = await getMeal(params.mealDetails);
+
+  let { title, slug, image, summary, instructions, creator, creator_email } =
+    meal;
+
   return (
-    <div>MealDetails</div>
-  )
+    <>
+      <header className={style.header}>
+        <div className={style.image}>
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={style.headerText}>
+          <h1>{title}</h1>
+          <p className={style.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={style.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={style.instructions}
+          dangerouslySetInnerHTML={{
+            __html: `${instructions.replace(/\n/g, "<br />")}`,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
